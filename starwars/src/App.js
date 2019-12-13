cffefa40addcee784ac5e2d52841745219caf7c5
Hiter Harris <hiterharris@comcast.net>
2019-12-13 11:17:42 -0500

    MVP complete

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,13 +3,7 @@ import Characters from './components/Characters';
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  
   return (
     <div className='app'>
       <h1 className='header'>React Wars</h1>
