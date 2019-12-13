5338f7701cd1aef23454874201415c10dfa1ac68
Hiter Harris <hiterharris@comcast.net>
2019-12-13 15:27:11 -0500

    removed unused code

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -62,24 +62,6 @@ const Characters = (props) => {
         </section>
     );
   }
-
-
-//   return (
-//       <section className='card-container'>
-//         {characters.map( (character) => {
-//             return (
-//                 <CardBody className='card'>
-//                     <CardTitle className='name'>{character.name}</CardTitle>
-//                     <CardText className='text'>Height: {character.height} cm</CardText>
-//                     <CardText className='text'>Weight: {character.mass} kg</CardText>
-//                     <CardText className='text'>Number of Starships: {character.starships.length}</CardText>
-//                     <CardText className='text'>Number of Vehicles: {character.vehicles.length}</CardText>
-//                     <CardText className='text'>Number of Films: {character.films.length}</CardText>
-//                 </CardBody>
-//             );
-//         })}
-//       </section>
-//   );
 };
 
 export default Characters;
